aa0d8ffe18330613ec9d1df67d708798ed50b2b8
danildenha <danildenha@gmail.com>
2024-04-14 13:18:32 -0500

    setting up all the files

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
       <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+        <div className="h-24"></div>
         <Navbar />
         {children}
       </div>
