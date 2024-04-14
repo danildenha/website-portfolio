1137a444052faf5dc8e5e8883fd0a66f03bb2963
danildenha <danildenha@gmail.com>
2024-04-14 13:58:55 -0500

    Managing navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,6 +1,23 @@
+import Link from "next/link"
+
 const Navbar = () => {
     return (
-        <div className=''>Navbar</div>
+        <div className='h-full flex items-center justify-between 
+        px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+        {/* LOGO WITH GRADIENT */}
+{/* LOGO WITH GRADIENT */}
+<div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+  <Link href="/" className="flex items-center justify-center">
+    <span className="text-gradient text-3xl font-extrabold tracking-wide">Denha</span>
+    <span className="ml-3 text-white text-sm rounded-md px-4 py-2 shadow-md" style={{ backgroundImage: 'linear-gradient(to right, #000000, #434343)' }}>portfolio</span>
+  </Link>
+</div>
+
+
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </div>
     )
 }
 
