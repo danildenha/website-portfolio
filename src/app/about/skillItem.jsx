d5afca8bb80b142bbb4a1362e77d25a184f15008
danildenha <danildenha@gmail.com>
2024-05-06 10:22:17 -0500

    change skill icons

--- src/app/about/skillItem.jsx
+++ src/app/about/skillItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SkillItem = ({ skill }) => {
   return (
-    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+    <div className="secondarySkills text-black border-radius-1.5">
       {skill}
     </div>
   );
