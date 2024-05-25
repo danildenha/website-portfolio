62b0be1138e6703f6b4a42a0a26518726c75a014
danildenha <danildenha@gmail.com>
2024-05-25 16:24:45 +0200

    changing code back

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,7 +24,7 @@ const Homepage = () => {
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            elcome to my digital canvas, where innovation and creativity
+            Welcome to my digital canvas, where innovation and creativity
             converge. With a keen eye for aesthetics and a mastery of code, my
             portfolio showcases a diverse collection of projects that reflect my
             commitment to excellence.
