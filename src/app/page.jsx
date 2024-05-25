810a4b1191b43ed0d19224854f2a7facecd68df6
danildenha <danildenha@gmail.com>
2024-05-25 09:34:05 +0200

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative mr-10">
-          <Image src="/hero.png" alt="" fill className="object-contain" />
+          <Image src="./public/hero.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
@@ -24,17 +24,17 @@ const Homepage = () => {
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Welcome to my digital canvas, where innovation and creativity
+            elcome to my digital canvas, where innovation and creativity
             converge. With a keen eye for aesthetics and a mastery of code, my
             portfolio showcases a diverse collection of projects that reflect my
             commitment to excellence.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <a href="/portfolio">
+            <a href="./portfolio">
             <button className="primary">View My Work</button>
             </a>
-            <a href="/contact">
+            <a href="./contact">
             <button className="secondary">Contact Me</button>
             </a>
 
