ff5283cd9351a99b24c5df6545cbf037d8c31812
danildenha <danildenha@gmail.com>
2024-05-25 09:47:34 +0200

    update basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: '/your-repo-name',
+  basePath: '/website-portfolio',
   images: {
     unoptimized: true,
     remotePatterns: [
