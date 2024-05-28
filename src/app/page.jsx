07cf93350af3f89cf6413fce277f9a85c1e77742
danildenha <danildenha@gmail.com>
2024-05-28 13:27:03 +0200

    changing icon links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full p-1 lg:w-1/2 relative">
-          <Image src="./hero.png" alt="" fill className="object-contain" />
+          <Image src="https://github.com/danildenha/website-portfolio/blob/584d981b8c5a6e86ea583337eee82be6d159e457/public/hero.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
